dd431b933d9d1b293fbab5544722453909c0378d
Reuben Kandiah <yes@kandoo.io>
2017-08-18 11:45:21 +0100

    Update capCollapse.r

--- capCollapse.r
+++ capCollapse.r
@@ -1,4 +1,9 @@
-# capCollapse Function looks for consecutive words beginning with capital letters and collapses them into a single word.
+# capCollapse Function looks for consecutive words beginning with capital letters and collapses them into a single word. Useful for grouping terms together for use in text mining.
+
+# Example
+# capCollapse("The current president of the United States is Donald Trump")
+# should return
+# [1] The current president of the UnitedStates is DonaldTrump
 
 capCollapse <- function(string){
   temp <- gsub('([A-Z]\\w*)', '\\1\\$MARK\\$', string)
@@ -7,4 +12,3 @@ capCollapse <- function(string){
   return(output)
 }
 
-capCollapse("The current president of the United States Donald Trump")
